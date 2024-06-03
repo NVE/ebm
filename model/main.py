c1a81b0032940b911e538d9a2c6dd4c2fb000390
Lars Fredrik Petrusson <lfep@nve.no>
2024-06-03 13:27:29 +0200

    adding changes to main

--- model/main.py
+++ model/main.py
@@ -4,14 +4,13 @@ from SCurvePerBuildingType import *
 
 import pandas as pd
 
-#input = DatabaseManager().s_curve_input_df
-#print(input)
+input = DatabaseManager().s_curve_input_df
 
-s = SCurvePerBuildingType('House')
+s = SCurvePerBuildingType('Apartment')
 
 ee = pd.DataFrame(s._s_curve_ee)
 rehab = pd.DataFrame(s._s_curve_rehab)
 demo = pd.DataFrame(s._s_curve_demolition)
 
-print(ee)
+print(input)
 

ec3608bef8c5f2af94731f8f0053022cd945187b
Ketil Nordstad <kenord@nve.no>
2024-06-21 08:20:59 +0200

    Implement script for testing r-sheets

---
+++ scripts/run_rehab.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import sys
+
+from loguru import logger
+
+from model.main import get_demolition_shares_per_tek
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--debug', action='store_true')
+    arguments = arg_parser.parse_args()
+
+    if not arguments.debug and os.environ.get('DEBUG', '') != 'True':
+        logger.remove()
+        logger.add(sys.stderr, level="INFO")
+
+    df = get_demolition_shares_per_tek('House')
+    #df = get_total_small_measures_shares_per_tek('House')
+    #df = get_total_renovation_shares_per_tek('House')
+    print(df)
+
+if __name__ == '__main__':
+    main()

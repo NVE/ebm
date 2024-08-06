e74c4c7962e8692453c17a5e4d576833368c2871
Ketil Nordstad <kenord@nve.no>
2024-08-06 09:02:01 +0200

    #1115 Load expected accumulated_constructed_floor_area from bema spreadsheet

--- scripts/run_construction.py
+++ scripts/run_construction.py
@@ -3,31 +3,29 @@ import collections.abc
 import os
 import sys
 
+from dotenv import load_dotenv
+from openpyxl import load_workbook, Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 from rich.pretty import pprint
 from loguru import logger
 
 from ebm.model import BuildingCategory
 from ebm.model.new_buildings import NewBuildings
+from model.bema import load_construction_building_category
 
 logger.info = pprint
 
 
 def load_expected_yearly_constructed(building_category: BuildingCategory) -> collections.abc.Collection:
-    if building_category == BuildingCategory.KINDERGARTEN:
-        return (97574, 188218, 254065, 316087, 396079, 470970, 536935, 599494, 653696, 707496, 761822, 797373,
-                831966, 866076, 896322, 923447, 947622, 968409, 987638, 1003524, 1018640, 1033603, 1048354,
-                1062933, 1077371, 1091662, 1105822, 1119722, 1133249, 1146405, 1159207, 1171662,
-                1184612, 1197210, 1211952, 1226342, 1240370, 1254043, 1267362, 1280335, 1292971,)
-    if building_category == BuildingCategory.UNIVERSITY:
-        return (112747, 161952, 206052, 240818, 325158, 375511, 418991, 459447, 494127, 528000, 561635, 584973, 608459,
-                632529, 655126, 676778, 697665, 717495, 739952, 761405, 785527, 809435, 833046, 856416, 879589, 902557,
-                925341, 947760, 969657, 991035, 1011918, 1032313, 1053229, 1073650, 1106477, 1138810, 1170638, 1201968,
-                1232802, 1263152, 1293028,)
+    load_dotenv()
+    spreadsheet_name = os.environ.get('BEMA_SPREADSHEET')
 
-    return (443334, 798437, 1447903, 1979251, 2299729, 2721889, 3087734, 3428423, 3722538, 4009570, 4294057, 4500971,
-            4712697, 4933270, 5145688, 5354382, 5561004, 5762916, 5993702, 6220151, 6536014, 6849630, 7160135, 7468127,
-            7774059, 8077834, 8379696, 8677741, 8970321, 9257475, 9539452, 9816326, 10099873, 10378254, 10802821,
-            11222223, 11636340, 12045246, 12448964, 12847624, 13241332,)
+    wb: Workbook = load_workbook(spreadsheet_name)
+    sheet_name: Worksheet = wb[wb.sheetnames[0]]
+
+    df = load_construction_building_category(sheet=sheet_name, building_category=building_category)
+
+    return df.accumulated_constructed_floor_area
 
 
 def main():
@@ -50,7 +48,7 @@ def main():
 
     error_count = 0
     for (year, cfa), expected_value in zip(construction_floor_area.items(), expected_values):
-        if round(cfa) != expected_value:
+        if round(cfa) != round(expected_value):
             error_count = error_count + 1
             logger.error(f'Expected {expected_value} was {round(cfa)} {building_category.name} {year=} {cfa=}')
             logger.debug(f'The difference is {expected_value - round(cfa)}')

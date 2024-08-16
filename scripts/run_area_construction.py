a3654108b800306f4059a906325a3042b0578d58
Ketil Nordstad <kenord@nve.no>
2024-08-16 09:44:10 +0200

    Simple refactor

--- scripts/run_area_construction.py
+++ scripts/run_area_construction.py
@@ -1,27 +1,34 @@
 """ Program to calculate total floor area including construction"""
+
 import pandas as pd
 
 from ebm.model import Buildings, BuildingCategory
 from ebm.model.construction import ConstructionCalculator
 from ebm.model.database_manager import DatabaseManager
 
-database_manager = DatabaseManager()
+START_YEAR = 2010
+END_YEAR = 2050
+YEARS = [y for y in range(START_YEAR, END_YEAR + 1)]
 
+database_manager = DatabaseManager()
 building_category = BuildingCategory.HOUSE
 
-buildings = Buildings.build_buildings(building_category, database_manager)
-area_forecast = buildings.build_area_forecast(database_manager, start_year=2010, end_year=2050)
+buildings = Buildings.build_buildings(building_category=building_category,
+                                      database_manager=database_manager)
 
-years = [y for y in range(2010, 2050 + 1)]
-demolition_floor_area = pd.Series(data=area_forecast.calc_total_demolition_area_per_year(), index=years)
+area_forecast = buildings.build_area_forecast(database_manager=database_manager,
+                                              start_year=START_YEAR,
+                                              end_year=END_YEAR)
+demolition_floor_area = area_forecast.calc_total_demolition_area_per_year()
 
-yearly_constructed = ConstructionCalculator.calculate_construction(building_category, demolition_floor_area, database_manager)
+yearly_constructed = ConstructionCalculator.calculate_construction(
+    building_category=building_category,
+    demolition_floor_area=pd.Series(data=demolition_floor_area, index=YEARS),
+    database_manager=database_manager)
 
-constructed_floor_area = yearly_constructed.accumulated_constructed_floor_area
-
-result = area_forecast.calc_area_with_construction([v for v in constructed_floor_area])
+result = area_forecast.calc_area_with_construction(yearly_constructed.accumulated_constructed_floor_area.tolist())
 
 for tek, building_condition in result.items():
-    df = pd.DataFrame(data=building_condition, index=[y for y in range(2010, 2051)])
+    df = pd.DataFrame(data=building_condition, index=YEARS)
     print(tek)
     print(df)

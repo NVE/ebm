9b44d512e0dcb54c4a51ab8af165d2979c1f63f3
lfep <lfep@nve.no>
2024-09-06 16:12:53 +0200

    #1243 added method to calculate shares for small_measure_total

--- ebm/model/shares_per_condition_new.py
+++ ebm/model/shares_per_condition_new.py
@@ -13,10 +13,13 @@ class SharesPerConditionNew():
     """
     
     # TODO: 
-    # - add start and end year as input arguments, as they should be defined outside the class
-    # - add negative values checks 
-    # - add checks to control calculations, for example control total values and that original condition should not exceed 100%
-    # - make code less repetative (e.g for loops on tek_list -> change to own method?)
+    # - add checks to control calculations:
+    #       - for example control total values and that original condition should not exceed 100%
+    #       _ negative values checks             
+    # - make code less repetative:
+    #       - ideally, there could be a calc_shares_tek() method, that takes building_condition as input
+    #       - then, there could be an own calc_shares() method, that loops trough the tek_list for a given building_condition
+    #       - make self.model_years to a pd.Series RangeIndex
 
     def __init__(self, 
                  tek_list: typing.List[str], 
@@ -40,7 +43,45 @@ class SharesPerConditionNew():
         self.never_share_renovation = never_shares[BuildingCondition.RENOVATION]
         self.never_share_demolition = never_shares[BuildingCondition.DEMOLITION]
 
-    def calc_shares_demolition_tek(self, tek) -> pd.Series:
+    def _scurve_per_model_year(self, tek:str, scurve: pd.Series) -> pd.Series:
+        """
+        Helper method to get scurve rates per model years relevant for the specified TEK. 
+        """
+        # Get the building year for the specific TEK
+        building_year = self.tek.get_building_year(tek)
+
+        # Convert the model_years range to a RangeIndex
+        model_years = pd.Index(self.model_years)
+
+        # Calculate the building age for each model year
+        building_age_per_year = pd.Series(model_years - building_year, index=model_years) 
+        
+        # Filter to only include valid ages
+        building_age_per_year = building_age_per_year[building_age_per_year > 0]
+
+        # Control that the building ages are present in the S-curve before filtering
+        if not building_age_per_year.isin(scurve.index).all():
+            missing_ages = building_age_per_year[~building_age_per_year.isin(scurve.index)]
+            msg = (
+                f"\n"
+                f"Building ages not present in S-curve while calculating shares.\n"
+                f"The building year of {tek} might not be compatible with the model horizon. Control this parameter.\n"
+                f"\n"
+                f" - The building year of {tek} is: {building_year}.\n"
+                f" - Building ages not present in S-curve: {missing_ages.to_list()}"
+            )
+            logger.error(msg)
+            raise ValueError(f"Building ages not present in S-curve index for {tek}.")
+
+        # Filter the scruve rates on the relevant building ages
+        scurve = scurve.loc[building_age_per_year.values]
+
+        # Assign the corresponding model years to the index of the filtered S-curve
+        scurve_per_year = scurve.set_axis(building_age_per_year.index, axis=0)
+
+        return scurve_per_year
+
+    def calc_shares_demolition_tek(self, tek: str) -> pd.Series:
         """
         Calculate the percentage share of area that is demolished over time for a specific TEK.
 
@@ -54,7 +95,7 @@ class SharesPerConditionNew():
 
         - The share is set to 0 in the model start year. The floor area of a TEK in the start year is considered
           to be the area at the end of the year. Therefore, no building measures should be taken in this year.  
-        - The share is set to 0 in model years before and up until the building year of the TEK. There should be
+        - The share is set to 0 in model years before and including the building year of the TEK. There should be
           no measures taken in the first year that the building is constructed. In other words, the first measure
           can only be taken when the building is one year old.
         - The share is set to the S-curve rate of the model year if that year corresponds to the year after the 
@@ -69,7 +110,6 @@ class SharesPerConditionNew():
         - pd.Series: A Pandas Series with the calculated demolition shares for the specific TEK, where the index 
                      represents the model years and the values are the respective demolition shares.
         """
-
         # Get the building year for the specific TEK
         building_year = self.tek.get_building_year(tek)
 
@@ -121,7 +161,7 @@ class SharesPerConditionNew():
         # Set share to 0 in the start year 
         shares.loc[model_years == self.model_start_year] = 0
 
-        # Set share to 0 in years before the the building_year + 1
+        # Set share to 0 in years before and including the year the building was constructed
         shares.loc[model_years <= building_year] = 0
 
         return shares
@@ -146,7 +186,38 @@ class SharesPerConditionNew():
 
         return tek_shares
 
+    def calc_shares_small_measure_total_tek(self, tek:str) -> pd.Series:
+        """
+        """
+        # Get the building year for the specific TEK
+        building_year = self.tek.get_building_year(tek)
 
+        # Retrieve S-curve rates per model year
+        scurve_per_year = self._scurve_per_model_year(tek, self.scurve_small_measure)
+
+        # Retrieve demolition shares per model year
+        condition_shares = self.calc_shares_demolition_tek(tek)
+
+        # Convert the model_years range to a RangeIndex
+        model_years = pd.Index(self.model_years)
+
+        # Calculate max limit for doing a renovation measure (small measure or renovation)
+        measure_limit = 1 - condition_shares - self.never_share_small_measure
+
+        # Define an empty shares series with model years as index
+        shares = pd.Series(0.0, index=model_years)
+
+        # Set share to the S-curve rate for the corresponding model year
+        shares[scurve_per_year.index] = scurve_per_year
+        
+        # Set share to the measure limit value if the S-curve rate exceeds the measure limit
+        shares[shares > measure_limit] = measure_limit
+
+        # Set share to 0 in years before and including the year the building was constructed
+        shares.loc[model_years <= building_year] = 0
+
+        return shares
+        
 if __name__ == '__main__':
 
     from ebm.model.database_manager import DatabaseManager
@@ -173,7 +244,10 @@ if __name__ == '__main__':
     tek_params = database_manager.get_tek_params(tek_list)
     shares = SharesPerConditionNew(tek_list, tek_params, scurves, never_shares)
     
-    #tek_shares = shares.calc_shares_demolition_tek(tek_list[0])
+    tek = tek_list[1]
+
     shares_per_tek = shares.calc_shares_demolition()
+    for tek in tek_list:
+        shares.calc_shares_small_measure_total_tek(tek)
+        logger.info(tek)
     
-    print(shares_per_tek)

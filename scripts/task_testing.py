ebba832e55427e4d98d031dd53152d2405154647
lfep <lfep@nve.no>
2024-09-18 13:07:20 +0200

    #1325 integrated refactored class with rest of the scripts (buildings and bema_validation). Added temporaty method to handle series in AreaForecast class

--- scripts/task_testing.py
+++ scripts/task_testing.py
@@ -13,7 +13,6 @@ from ebm.model.building_category import BuildingCategory
 from ebm.model.filter_scurve_params import FilterScurveParams
 from ebm.model.scurve_processor import ScurveProcessor
 from ebm.model.filter_tek import FilterTek
-from ebm.model.shares_per_condition_new import SharesPerConditionNew
 from ebm.model.shares_per_condition import SharesPerCondition
 
 from ebm.model.bema_validation import *
@@ -86,37 +85,6 @@ def compare_nested_dict_of_series(dict1, dict2):
                 print(f'Not equal: {condition}, {tek}')
 
 
-# Refactor Shares datatype
-
-def create_shares_objects(building_category):
-    database_manager = DatabaseManager()
-    
-    scurve_condition_list = BuildingCondition.get_scruve_condition_list()
-    scurve_data_params = database_manager.get_scurve_params()
-    scurve_params = FilterScurveParams.filter(building_category, scurve_condition_list, scurve_data_params)
-    scurve_processor = ScurveProcessor(scurve_condition_list, scurve_params)
-    scurves = scurve_processor.get_scurves()
-    never_shares = scurve_processor.get_never_shares()
-
-    tek_list = FilterTek.get_filtered_list(building_category, database_manager.get_tek_list())
-    tek_params = database_manager.get_tek_params(tek_list)
-
-    shares_new = SharesPerConditionNew(tek_list, tek_params, scurves, never_shares)
-    shares_old = SharesPerCondition(tek_list, tek_params, scurves, never_shares)
-
-    return shares_new, shares_old
-
-def compare_shares_one_condition():
-    logger.info(building_category)
-    shares_new, shares_old = create_shares_objects(building_category)
-
-    dict1 = shares_new.calc_shares_all_conditions_teks("demolition")
-    dict2 = shares_old.calc_shares_demolition()
-    dict2 = dict_with_lists_to_series(dict2)
-    compare_dict_of_series(dict1, dict2)
-
-
-
 def create_shares_object(building_category):
     database_manager = DatabaseManager()
     
@@ -130,9 +98,21 @@ def create_shares_object(building_category):
     tek_list = FilterTek.get_filtered_list(building_category, database_manager.get_tek_list())
     tek_params = database_manager.get_tek_params(tek_list)
 
-    shares = SharesPerConditionNew(tek_list, tek_params, scurves, never_shares)
+    shares = SharesPerCondition(tek_list, tek_params, scurves, never_shares)
     return shares
 
-shares= create_shares_object(building_category)
-shares_condition = shares.calc_shares_all_conditions_teks()
+#shares= create_shares_object(building_category)
+#shares_condition = shares.calc_shares_all_conditions_teks()
 
+
+modelyears = YearRange(2010, 2050)
+
+building = Buildings.build_buildings(building_category, database_manager)
+area_forecast = building.build_area_forecast(database_manager)
+
+demolition_floor_area = pd.Series(data=area_forecast.calc_total_demolition_area_per_year(), index=modelyears)
+yearly_constructed = ConstructionCalculator.calculate_construction(building_category, demolition_floor_area,
+                                                                    database_manager, modelyears)
+constructed_floor_area = list(yearly_constructed.accumulated_constructed_floor_area)
+area = area_forecast.calc_area(constructed_floor_area)
+#print(area)

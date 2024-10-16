3a2fe18152d72222a933a47baaf8882f8b3fb6cf
Ketil Nordstad <kenord@nve.no>
2024-10-16 09:53:14 +0200

    #1073 Remove redundant module greier

--- scripts/energibehov.py
+++ scripts/energibehov.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 from ebm.__main__ import calculate_building_category_area_forecast
-from ebm.greier import load_heating_reduction, load_energy_by_floor_area, load_area_forecast
 from ebm.model import BuildingCategory, DatabaseManager
 from ebm.model.bema import filter_existing_area, calculate_area_distribution
 from ebm.model.building_condition import BuildingCondition
@@ -10,6 +9,49 @@ from ebm.model.energy_requirement import (calculate_energy_requirement_reduction
                                           calculate_energy_requirement_reduction,
                                           calculate_lighting_reduction)
 from ebm.services.console import rich_display_dataframe
+
+
+def load_heating_reduction(purpose='heating_rv'):
+    def make_zero_reduction():
+        r = [{'building_condition': condition, 'reduction': 0} for condition in BuildingCondition if condition != BuildingCondition.DEMOLITION]
+        return r
+    heating_reduction = pd.read_csv('input/energy_requirement_reduction_per_condition.csv')
+    if purpose != 'heating_rv':
+        return pd.DataFrame(data=make_zero_reduction())
+    heating_reduction['reduction'] = heating_reduction['reduction_share']
+    heating_reduction = heating_reduction[heating_reduction.TEK == 'default'][['building_condition', 'reduction']]
+    return heating_reduction
+
+
+def load_energy_by_floor_area(building_category, purpose='heating_rv'):
+    energy_by_floor_area = pd.read_csv('input/energy_requirement_original_condition.csv')
+    df = energy_by_floor_area[(energy_by_floor_area.building_category == building_category) &
+                              (energy_by_floor_area.purpose == purpose)]
+    return df
+
+
+def load_area_forecast(building_category: BuildingCategory = BuildingCategory.KINDERGARTEN) -> pd.DataFrame:
+    dm = DatabaseManager()
+    area = calculate_building_category_area_forecast(building_category=building_category,
+                                                     database_manager=dm,
+                                                     start_year=2010,
+                                                     end_year=2050)
+
+    data = {'building_category': [], 'TEK': [], 'building_condition': [], 'year': [], 'area': []}
+    for tek, item in area.items():
+        for condition, years in item.items():
+            if condition == BuildingCondition.DEMOLITION:
+                continue
+            for year, area in enumerate(years, start=2010):
+                data.get('building_category').append(building_category)
+                data.get('TEK').append(tek)
+                data.get('building_condition').append(condition)
+                data.get('year').append(year)
+                data.get('area').append(area)
+
+    area_forecast = pd.DataFrame(data=data)
+    return area_forecast
+
 
 
 def distribute_energy_requirement_over_area(area_forecast, requirement_by_condition):

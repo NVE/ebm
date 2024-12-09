9b7f7708a4b710e8349cb24023bc06e55c5b6eb8
lfep <lfep@nve.no>
2024-12-09 14:44:57 +0100

    #2011 refactored building_category and building category group

--- ebm/heating_systems_projection.py
+++ ebm/heating_systems_projection.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from ebm.model.building_category import BuildingCategory, RESIDENTIAL, NON_RESIDENTIAL
+
 BUILDING_CATEGORY = 'building_category'
 TEK = 'TEK'
 HEATING_SYSTEMS = 'Oppvarmingstyper'
@@ -7,7 +9,8 @@ NEW_HEATING_SYSTEMS = 'Nye_oppvarmingstyper'
 YEAR = 'Year'
 TEK_SHARES = 'TEK_andeler'
 
-def legge_til_alle_oppvarmingstyper(heating_systems_shares, heating_systems_efficiencies):
+def legge_til_alle_oppvarmingstyper(heating_systems_shares: pd.DataFrame, 
+                                    heating_systems_efficiencies: pd.DataFrame) -> pd.DataFrame:
     df_aggregert_0 = heating_systems_shares.copy()
     oppvarmingstyper = heating_systems_efficiencies[[HEATING_SYSTEMS]].copy()
 
@@ -26,7 +29,8 @@ def legge_til_alle_oppvarmingstyper(heating_systems_shares, heating_systems_effi
     
     return df_aggregert_alle_kombinasjoner
 
-def legge_til_ulike_oppvarmingslaster(df, heating_systems_efficiencies):
+def legge_til_ulike_oppvarmingslaster(df: pd.DataFrame, 
+                                      heating_systems_efficiencies: pd.DataFrame) -> pd.DataFrame:
     df_hoved_spiss_og_ekstralast = heating_systems_efficiencies.copy()
     df_oppvarmingsteknologier_andeler = df.merge(df_hoved_spiss_og_ekstralast, on = [HEATING_SYSTEMS], 
                                                  how ='left')
@@ -34,29 +38,33 @@ def legge_til_ulike_oppvarmingslaster(df, heating_systems_efficiencies):
     return df_oppvarmingsteknologier_andeler
 
 def aggregere_lik_oppvarming_fjern_0(df):
-    df_fjern_null = df.query("TEK_andeler != 0").copy()
+    df_fjern_null = df.query(f"{TEK_SHARES} != 0").copy()
     df_aggregert = df_fjern_null.groupby([BUILDING_CATEGORY, TEK, HEATING_SYSTEMS, YEAR], 
                                          as_index = False)[TEK_SHARES].sum()
     return df_aggregert
 
-def ekspandere_input_oppvarming(heating_systems_forecast):
-    alle_bygningskategorier = "House+Apartment block+Kindergarten+Office+Retail+Hotel+Sports+University+School+Nursinghome+Hospital+Storage repairs+Culture"
-    alle_tek = "pre_TEK49+TEK49+TEK69+TEK87+TEK97+TEK07+TEK10+TEK17"
-    husholdning = "House+Apartment block"
-    yrkesbygg = "Kindergarten+Office+Retail+Hotel+Sports+University+School+Nursinghome+Hospital+Storage repairs+Culture"
+def expand_building_category_tek(heating_systems_forecast: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds necessary building categories and TEK's to the heating_systems_forecast dataframe. 
+    """
+    alle_bygningskategorier = '+'.join(BuildingCategory)
+    alle_tek = "PRE_TEK49+TEK49+TEK69+TEK87+TEK97+TEK07+TEK10+TEK17"
+    husholdning = '+'.join(bc for bc in BuildingCategory if bc.is_residential())
+    yrkesbygg = '+'.join(bc for bc in BuildingCategory if bc.is_non_residential())
     
     df = heating_systems_forecast.copy()
     df.loc[df[TEK] == "default", TEK] = alle_tek
     df.loc[df[BUILDING_CATEGORY] == "default", BUILDING_CATEGORY] = alle_bygningskategorier
-    df.loc[df[BUILDING_CATEGORY] == "Household", BUILDING_CATEGORY] = husholdning
-    df.loc[df[BUILDING_CATEGORY] == "Non-residential", BUILDING_CATEGORY] = yrkesbygg
+    df.loc[df[BUILDING_CATEGORY] == RESIDENTIAL, BUILDING_CATEGORY] = husholdning
+    df.loc[df[BUILDING_CATEGORY] == NON_RESIDENTIAL, BUILDING_CATEGORY] = yrkesbygg
 
     df = df.assign(**{BUILDING_CATEGORY: df[BUILDING_CATEGORY].str.split('+')}).explode(BUILDING_CATEGORY)
     df2 = df.assign(**{TEK: df[TEK].str.split('+')}).explode(TEK)
     df2 = df2.reset_index(drop = True)
     return df2
 
-def framskrive_oppvarming(df, inputfil):
+def framskrive_oppvarming(df: pd.DataFrame, 
+                          inputfil: pd.DataFrame) -> pd.DataFrame:
     inputfil_oppvarming = inputfil
 
     df_framskrive_oppvarming_long = inputfil_oppvarming.melt(id_vars = [BUILDING_CATEGORY, TEK, HEATING_SYSTEMS, NEW_HEATING_SYSTEMS],
@@ -64,31 +72,31 @@ def framskrive_oppvarming(df, inputfil):
     liste_eksisterende_oppvarming = list(df_framskrive_oppvarming_long[HEATING_SYSTEMS].unique())
     liste_ny_oppvarming = list(df_framskrive_oppvarming_long[NEW_HEATING_SYSTEMS].unique())
 
-    oppvarming_og_TEK = df.query(f"Oppvarmingstyper == {liste_eksisterende_oppvarming}")[[BUILDING_CATEGORY,TEK, HEATING_SYSTEMS, YEAR, TEK_SHARES]].copy()
-    oppvarming_og_TEK_foer_endring = df.query(f"Oppvarmingstyper == {liste_ny_oppvarming}")[[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS, TEK_SHARES]].copy()
+    oppvarming_og_TEK = df.query(f"{HEATING_SYSTEMS} == {liste_eksisterende_oppvarming}")[[BUILDING_CATEGORY,TEK, HEATING_SYSTEMS, YEAR, TEK_SHARES]].copy()
+    oppvarming_og_TEK_foer_endring = df.query(f"{HEATING_SYSTEMS} == {liste_ny_oppvarming}")[[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS, TEK_SHARES]].copy()
 
     df_merge = oppvarming_og_TEK.merge(df_framskrive_oppvarming_long, on = [BUILDING_CATEGORY,TEK,HEATING_SYSTEMS], how = 'inner')
     df_merge['Ny_andel'] = (df_merge[TEK_SHARES]*
                             df_merge['Andel_utskiftning'])
 
-    df_ny_andel_sum = df_merge.groupby([BUILDING_CATEGORY, TEK,HEATING_SYSTEMS, 'Year_y'], as_index = False)[['Ny_andel']].sum()
+    df_ny_andel_sum = df_merge.groupby([BUILDING_CATEGORY, TEK,HEATING_SYSTEMS, f'{YEAR}_y'], as_index = False)[['Ny_andel']].sum()
     df_ny_andel_sum = df_ny_andel_sum.rename(columns = {"Ny_andel": "Sum_ny_andel"})
 
-    df_merge_sum_ny_andel = pd.merge(df_merge, df_ny_andel_sum, on = [BUILDING_CATEGORY,TEK,HEATING_SYSTEMS, 'Year_y'])
+    df_merge_sum_ny_andel = pd.merge(df_merge, df_ny_andel_sum, on = [BUILDING_CATEGORY,TEK,HEATING_SYSTEMS, f'{YEAR}_y'])
 
     df_merge_sum_ny_andel['Eksisterende_andel'] = ((df_merge_sum_ny_andel[TEK_SHARES] -
                                                     df_merge_sum_ny_andel['Sum_ny_andel']))
 
-    kolonner_eksisterende = ['Year_y', BUILDING_CATEGORY, TEK, 'Eksisterende_andel', HEATING_SYSTEMS]
+    kolonner_eksisterende = [f'{YEAR}_y', BUILDING_CATEGORY, TEK, 'Eksisterende_andel', HEATING_SYSTEMS]
     navn_eksisterende_kolonner = {"Eksisterende_andel" : TEK_SHARES,
                                     NEW_HEATING_SYSTEMS : HEATING_SYSTEMS, 
-                                    "Year_y" : YEAR}
+                                    f'{YEAR}_y' : YEAR}
 
 
-    kolonner_nye = ['Year_y', BUILDING_CATEGORY, TEK, 'Ny_andel', NEW_HEATING_SYSTEMS]
+    kolonner_nye = [f'{YEAR}_y', BUILDING_CATEGORY, TEK, 'Ny_andel', NEW_HEATING_SYSTEMS]
     navn_nye_kolonner = {"Ny_andel" : TEK_SHARES, 
                         NEW_HEATING_SYSTEMS : HEATING_SYSTEMS, 
-                        "Year_y" : YEAR}
+                        f'{YEAR}_y' : YEAR}
 
     rekkefolge_kolonner = [YEAR, BUILDING_CATEGORY, TEK, HEATING_SYSTEMS, TEK_SHARES]
 
@@ -98,8 +106,8 @@ def framskrive_oppvarming(df, inputfil):
     nye_andeler_nye = aggregere_lik_oppvarming_fjern_0(nye_andeler_nye)
 
     nye_andeler_pluss_eksisterende = nye_andeler_nye.merge(oppvarming_og_TEK_foer_endring, on = [BUILDING_CATEGORY,TEK,HEATING_SYSTEMS], how = 'inner')
-    nye_andeler_pluss_eksisterende[TEK_SHARES] = nye_andeler_pluss_eksisterende['TEK_andeler_x'] + nye_andeler_pluss_eksisterende['TEK_andeler_y']
-    nye_andeler_pluss_eksisterende = nye_andeler_pluss_eksisterende.drop(columns = ['TEK_andeler_x', 'TEK_andeler_y'])
+    nye_andeler_pluss_eksisterende[TEK_SHARES] = nye_andeler_pluss_eksisterende[f'{TEK_SHARES}_x'] + nye_andeler_pluss_eksisterende[f'{TEK_SHARES}_y']
+    nye_andeler_pluss_eksisterende = nye_andeler_pluss_eksisterende.drop(columns = [f'{TEK_SHARES}_x', f'{TEK_SHARES}_y'])
 
     nye_andeler_samlet = pd.concat([nye_andeler_eksisterende, nye_andeler_pluss_eksisterende])
     nye_andeler_drop_dupe = nye_andeler_samlet.drop_duplicates(subset = [YEAR, BUILDING_CATEGORY, TEK, HEATING_SYSTEMS, TEK_SHARES], keep = 'first')
@@ -108,7 +116,8 @@ def framskrive_oppvarming(df, inputfil):
 
     return nye_andeler_samlet_uten_0
 
-def legge_til_resterende_aar_oppvarming(df_nye_andeler, df_eksisterende):
+def legge_til_resterende_aar_oppvarming(df_nye_andeler: pd.DataFrame,
+                                        df_eksisterende: pd.DataFrame) -> pd.DataFrame:
     df_nye_andeler_kopi = df_nye_andeler.copy()
 
     def sortering_oppvarmingstyper(df):
@@ -134,16 +143,16 @@ def legge_til_resterende_aar_oppvarming(df_nye_andeler, df_eksisterende):
     samlede_nye_andeler = samlede_nye_andeler.drop(columns = ['Sortering'])
     return samlede_nye_andeler
 
-def main(heating_systems_shares,
-         heating_systems_efficiencies,
-         heating_systems_forecast):
+def main(heating_systems_shares: pd.DataFrame,
+         heating_systems_efficiencies: pd.DataFrame,
+         heating_systems_forecast: pd.DataFrame) -> pd.DataFrame:
     
     # Legger til 0 på oppvarmingstyper som ikke eksisterer enda.
     df_aggregert_alle_kombinasjoner = legge_til_alle_oppvarmingstyper(heating_systems_shares, 
                                                                       heating_systems_efficiencies)
 
     # Gjør klar inputfilen for oppvarmingsandelene som skal framskrives.
-    inputfil_oppvarming = ekspandere_input_oppvarming(heating_systems_forecast)
+    inputfil_oppvarming = expand_building_category_tek(heating_systems_forecast)
 
     # Framskriver oppvarmingsløsninger basert på andelene i input filen
     nye_andeler = framskrive_oppvarming(df_aggregert_alle_kombinasjoner, inputfil_oppvarming)
--- tests/ebm/test_heating_systems_forecast.py
+++ tests/ebm/test_heating_systems_forecast.py
@@ -3,11 +3,12 @@ import typing
 import pandas as pd
 import pytest
 
+from ebm.model.building_category import BuildingCategory
 from ebm.model.heating_systems import HeatingSystems
 from ebm.heating_systems_projection import (legge_til_alle_oppvarmingstyper,
                                           legge_til_ulike_oppvarmingslaster,
                                           aggregere_lik_oppvarming_fjern_0,
-                                          ekspandere_input_oppvarming,
+                                          expand_building_category_tek,
                                           framskrive_oppvarming,
                                           legge_til_resterende_aar_oppvarming,
                                           main)
@@ -26,12 +27,12 @@ TEK_SHARES = 'TEK_andeler'
 @pytest.fixture
 def shares() -> pd.DataFrame:
     return pd.read_csv(io.StringIO("""
-Kindergarten,TEK97,DH,2020,0.1832382347789622
-Kindergarten,TEK97,Electric boiler,2020,0.5487908275136798
-Kindergarten,TEK97,Electricity,2020,0.1352944639341367
-Kindergarten,TEK97,Electricity - Bio,2020,0.01932495652066477
-Kindergarten,TEK97,Gas,2020,0.07299731601863742
-Kindergarten,TEK97,HP Central heating,2020,0.04035420123391918                                                                                                      
+kindergarten,TEK97,DH,2020,0.1832382347789622
+kindergarten,TEK97,Electric boiler,2020,0.5487908275136798
+kindergarten,TEK97,Electricity,2020,0.1352944639341367
+kindergarten,TEK97,Electricity - Bio,2020,0.01932495652066477
+kindergarten,TEK97,Gas,2020,0.07299731601863742
+kindergarten,TEK97,HP Central heating,2020,0.04035420123391918                                                                                                      
 """.strip()),
 names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,YEAR,TEK_SHARES], skipinitialspace=True)
 
@@ -62,11 +63,11 @@ def forecast() -> pd.DataFrame:
     return pd.read_csv(io.StringIO("""
 default,default,Gas,HP Central heating,0.05,0.075
 default,default,Gas,Electric boiler,0.05,0.075
-Non-residential,TEK69,HP Central heating - Gas,HP Central heating,0.1,0.15
+non_residential,TEK69,HP Central heating - Gas,HP Central heating,0.1,0.15
 House,default,Electricity - Bio,HP - Bio,0.05000000000000001,0.05489399966624506
 Apartment block,default,Electricity - Bio,HP Central heating - Bio,0.05000000000000001,0.05489399966624506
-Household,default,HP - Electricity,HP - Bio,0.05000000000000001,0.055016300447270426
-Kindergarten+Office,TEK87+TEK97,Electricity,DH,0.05000000000000001,0.055016300447270426        
+residential,default,HP - Electricity,HP - Bio,0.05000000000000001,0.055016300447270426
+kindergarten+Office,TEK87+TEK97,Electricity,DH,0.05000000000000001,0.055016300447270426        
 """.strip()),
 names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,NEW_HEATING_SYSTEMS,2021,2022] ,skipinitialspace=True)
 
@@ -102,27 +103,75 @@ Kindergarten,TEK97,HP Central heating - Bio,2020,0.0
     pd.testing.assert_frame_equal(result, expected)
 
 
+# TODO: add tests to check if the different building categories are set properly. TEK??
+def test_expand_building_categoy_tek_all_categories():
+    """
+    Checks if all categories are added if building_category value == default.
+    """
+    forecast = pd.read_csv(io.StringIO("""
+default,default,gas,DH,0.05,0.075    
+""".strip()),
+names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,NEW_HEATING_SYSTEMS,2021,2022] ,skipinitialspace=True)
+    result = expand_building_category_tek(forecast)
+    result_categories = result[BUILDING_CATEGORY].unique()
+    expected_categories = ['house', 'apartment_block', 'kindergarten', 'school', 'university', 'office', 'retail',
+                           'hotel', 'hospital', 'nursing_home', 'culture', 'sports', 'storage_repairs']
+    invalid_categories = [bc for bc in result_categories if bc not in expected_categories]
+    assert not invalid_categories
+
+
+def test_expand_building_categoy_tek_residential_categories():
+    """
+    Checks if correct categories are added if building_category value == residential.
+    """
+    forecast = pd.read_csv(io.StringIO("""
+residential,default,gas,DH,0.05,0.075    
+""".strip()),
+names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,NEW_HEATING_SYSTEMS,2021,2022] ,skipinitialspace=True)
+    result = expand_building_category_tek(forecast)
+    result_categories = result[BUILDING_CATEGORY].unique()
+    residential_categories = ['house', 'apartment_block']
+    invalid_categories = [bc for bc in result_categories if bc not in residential_categories]
+    assert not invalid_categories
+
+
+def test_expand_building_categoy_tek_non_residential_categories():
+    """
+    Checks if correct categories are added if building_category value == non_residential.
+    """
+    forecast = pd.read_csv(io.StringIO("""
+non_residential,default,gas,DH,0.05,0.075    
+""".strip()),
+names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,NEW_HEATING_SYSTEMS,2021,2022] ,skipinitialspace=True)
+    result = expand_building_category_tek(forecast)
+    result_categories = result[BUILDING_CATEGORY].unique()
+    non_residential_categories = ['kindergarten', 'school', 'university', 'office', 'retail', 'hotel',
+                                  'hospital', 'nursing_home', 'culture', 'sports', 'storage_repairs']
+    invalid_categories = [bc for bc in result_categories if bc not in non_residential_categories]
+    assert not invalid_categories
+
+
 def test_main_ok(shares, efficiencies, forecast):
 
     expected = pd.read_csv(io.StringIO("""
-Kindergarten,TEK97,Electricity - Bio,2021,0.01932495652066477,0
-Kindergarten,TEK97,Electricity - Bio,2022,0.01932495652066477,0
-Kindergarten,TEK97,DH,2021,0.19000295797566905,0
-Kindergarten,TEK97,DH,2022,0.19068163565561505,0
-Kindergarten,TEK97,Electric boiler,2021,0.5524406933146117,0
-Kindergarten,TEK97,Electric boiler,2022,0.5542656262150776,0
-Kindergarten,TEK97,Electricity,2021,0.12852974073742984,0
-Kindergarten,TEK97,Electricity,2022,0.12785106305748384,0
-Kindergarten,TEK97,Gas,2021,0.06569758441677367,0
-Kindergarten,TEK97,Gas,2022,0.0620477186158418,0
-Kindergarten,TEK97,HP Central heating,2021,0.044004067034851053,0
-Kindergarten,TEK97,HP Central heating,2022,0.045828999935316986,0
-Kindergarten,TEK97,DH,2020,0.1832382347789622,0
-Kindergarten,TEK97,Electric boiler,2020,0.5487908275136798,0
-Kindergarten,TEK97,Electricity,2020,0.1352944639341367,0
-Kindergarten,TEK97,Electricity - Bio,2020,0.01932495652066477,0
-Kindergarten,TEK97,Gas,2020,0.07299731601863742,0
-Kindergarten,TEK97,HP Central heating,2020,0.04035420123391918,0
+kindergarten,TEK97,Electricity - Bio,2021,0.01932495652066477,0
+kindergarten,TEK97,Electricity - Bio,2022,0.01932495652066477,0
+kindergarten,TEK97,DH,2021,0.19000295797566905,0
+kindergarten,TEK97,DH,2022,0.19068163565561505,0
+kindergarten,TEK97,Electric boiler,2021,0.5524406933146117,0
+kindergarten,TEK97,Electric boiler,2022,0.5542656262150776,0
+kindergarten,TEK97,Electricity,2021,0.12852974073742984,0
+kindergarten,TEK97,Electricity,2022,0.12785106305748384,0
+kindergarten,TEK97,Gas,2021,0.06569758441677367,0
+kindergarten,TEK97,Gas,2022,0.0620477186158418,0
+kindergarten,TEK97,HP Central heating,2021,0.044004067034851053,0
+kindergarten,TEK97,HP Central heating,2022,0.045828999935316986,0
+kindergarten,TEK97,DH,2020,0.1832382347789622,0
+kindergarten,TEK97,Electric boiler,2020,0.5487908275136798,0
+kindergarten,TEK97,Electricity,2020,0.1352944639341367,0
+kindergarten,TEK97,Electricity - Bio,2020,0.01932495652066477,0
+kindergarten,TEK97,Gas,2020,0.07299731601863742,0
+kindergarten,TEK97,HP Central heating,2020,0.04035420123391918,0
 """.strip()),
 names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,YEAR,TEK_SHARES,'Value'] ,skipinitialspace=True)
     
@@ -130,9 +179,6 @@ names=[BUILDING_CATEGORY,TEK,HEATING_SYSTEMS,YEAR,TEK_SHARES,'Value'] ,skipiniti
     result = result[result[YEAR].isin([2020,2021,2022])]
     result.reset_index(drop=True, inplace=True)
     pd.testing.assert_frame_equal(result, expected)
-
-
-
 
 
 if __name__ == "__main__":

db05fc9f853c710820035b5b19acfc8e9f0e782b
kenord <kenord@nve.no>
2025-01-16 08:52:29 +0100

    #2195 Write different levels of aggregated to different sheets
     - Write different levels of aggregated to different sheets
     - Refactor __main__ add function extract_report

--- ebm/__main__.py
+++ ebm/__main__.py
@@ -8,14 +8,16 @@ from loguru import logger
 
 from ebm.cmd.calibrate import run_calibration
 from ebm.model.calibrate_energy_requirements import create_heating_rv
-from ebm.cmd.run_calculation import calculate_building_category_area_forecast, \
-    result_to_horizontal_dataframe, make_arguments, validate_years, \
+from ebm.cmd.run_calculation import calculate_building_category_area_forecast,  make_arguments, validate_years, \
     calculate_building_category_energy_requirements, calculate_heating_systems, calculate_energy_use, configure_loglevel
 from ebm.model.building_category import BuildingCategory
 from ebm.model.building_condition import BuildingCondition
 from ebm.model.database_manager import DatabaseManager
 from ebm.model.file_handler import FileHandler
 
+from ebm.model.building_condition import BEMA_ORDER as building_condition_order
+from ebm.model.building_category import BEMA_ORDER as building_category_order
+from ebm.model.tek import BEMA_ORDER as tek_order
 
 def main() -> int:
     """
@@ -83,73 +85,112 @@ You can overwrite the {output_file}. by using --force: {program_name} {' '.join(
 """.strip(),
               file=sys.stderr)
         return 1
-    if not file_is_writable(output_file):
+    if output_file.name != '-' and not file_is_writable(output_file):
         logger.error(f'{output_file} is not writable')
         return 2
 
     logger.info('Loading area forecast')
 
-    output = None
+    model = None
     calibration_year = arguments.calibration_year
     step_choice = arguments.step
+    transform_to_horizontal_years = arguments.horizontal_years
 
     if calibration_year:
-        output = run_calibration(database_manager, calibration_year)
+        model = run_calibration(database_manager, calibration_year)
         calibration_directory = pathlib.Path('kalibrering')
         if not calibration_directory.is_dir():
             calibration_directory.mkdir()
         calibration_manager = DatabaseManager(FileHandler(directory=calibration_directory))
         calibration_manager.file_handler.create_missing_input_files()
         create_heating_rv(calibration_manager)
-
+    elif step_choice == 'energy-use':
+        model = calculate_energy_use()
     else:
         for building_category in building_categories:
-            if building_category == BuildingCategory.STORAGE_REPAIRS:
-                continue
-            if step_choice == 'energy-use':
-                output = calculate_energy_use()
-                break
+            df = extract_model(arguments, building_category, building_conditions, database_manager, step_choice)
+            if model is None:
+                model = df
             else:
-                area_forecast_result = calculate_building_category_area_forecast(
-                    building_category=building_category,
-                    database_manager=database_manager,
-                    start_year=arguments.start_year,
-                    end_year=arguments.end_year)
+                model = pd.concat([model, df])
 
-                df = area_forecast_result
-                df = df.set_index(['building_category', 'TEK', 'building_condition', 'year'])
-
-                if building_conditions:
-                    df = df.loc[:, :, [str(s) for s in building_conditions]]
-                if arguments.tek:
-                    tek_in_index = [t for t in arguments.tek if any(df.index.isin([t], level=1))]
-                    df = df.loc[:, tek_in_index, :]
-
-            if 'energy-requirements' in step_choice or 'heating-systems' in step_choice:
-                energy_requirements_result = calculate_building_category_energy_requirements(
-                    building_category=building_category,
-                    area_forecast=df,
-                    database_manager=database_manager,
-                    start_year=arguments.start_year,
-                    end_year=arguments.end_year)
-                df = energy_requirements_result
-                if 'heating-systems' in step_choice and building_category != BuildingCategory.STORAGE_REPAIRS:
-                    df = calculate_heating_systems(energy_requirements=energy_requirements_result,
-                                                   database_manager=database_manager)
-
-            if output is None:
-                output = df
-            else:
-                output = pd.concat([output, df])
-        transform_to_horizontal_years = arguments.horizontal_years
-        if transform_to_horizontal_years:
-            output = result_to_horizontal_dataframe(output)
-
-    write_result(output_file, csv_delimiter, output)
+    if transform_to_horizontal_years and (step_choice in ['area-forecast', 'energy-requirements']) and output_file.suffix=='.xlsx':
+        write_horizontal_excel(output_file, model)
+    else:
+        write_result(output_file, csv_delimiter, model)
 
     if arguments.open:
         os.startfile(output_file, 'open')
     sys.exit(0)
+
+
+def write_horizontal_excel(output_file: pathlib.Path, model: pd.DataFrame):
+    logger.info(f'write horizontal {output_file}')
+    hz = model.reset_index().copy()
+    value_column = 'energy_requirement' if 'energy_requirement' in hz.columns else 'm2'
+    hz = hz.groupby(by=['building_category', 'TEK', 'building_condition', 'year'], as_index=False).sum()[
+        ['building_category', 'TEK', 'building_condition', 'year', value_column]]
+    hz = hz.pivot(columns=['year'], index=['building_category', 'TEK', 'building_condition'], values=[
+        value_column]).reset_index()
+
+    hz = hz.sort_values(by=['building_category', 'TEK', 'building_condition'],
+                        key=lambda x: x.map(building_category_order) if x.name == 'building_category' else x.map(
+                            tek_order) if x.name == 'TEK' else x.map(
+                            building_condition_order) if x.name == 'building_condition' else x)
+    hz.columns = ['building_category', 'TEK', 'building_condition'] + [y for y in range(2020, 2051)]
+
+    by_tek = model.reset_index().copy().groupby(by=['building_category', 'TEK', 'year'], as_index=False).sum()[
+        ['building_category', 'TEK', 'year', value_column]]
+    by_tek = by_tek.pivot(columns=['year'], index=['building_category', 'TEK']).reset_index()
+
+    by_tek = by_tek.sort_values(by=['building_category', 'TEK'],
+                                key=lambda x: x.map(
+                                    building_category_order) if x.name == 'building_category' else x.map(
+                                    tek_order) if x.name == 'TEK' else x)
+    by_tek.columns = ['building_category', 'TEK'] + [y for y in range(2020, 2051)]
+
+    by_bc = model.reset_index().copy().groupby(by=['building_category', 'year'], as_index=False).sum()[
+        ['building_category', 'year', value_column]]
+    by_bc = by_bc.pivot(columns=['year'], index=['building_category'], values=value_column).reset_index()
+    by_bc = by_bc.sort_values(by=['building_category'],
+                              key=lambda x: x.map(building_category_order) if x.name == 'building_category' else x)
+    by_bc.columns = ['building_category'] + [y for y in range(2020, 2051)]
+
+    with pd.ExcelWriter(output_file, mode='w', engine='openpyxl') as writer:
+        hz.to_excel(writer, sheet_name='condition', index=False, startcol=2)
+        by_tek.to_excel(writer, sheet_name='TEK', index=False, startcol=2)
+        by_bc.to_excel(writer, sheet_name='category', index=False, startcol=2)
+
+
+def extract_model(arguments, building_category, building_conditions, database_manager, step_choice):
+    if building_category == BuildingCategory.STORAGE_REPAIRS:
+        pass
+    area_forecast_result = calculate_building_category_area_forecast(
+        building_category=building_category,
+        database_manager=database_manager,
+        start_year=arguments.start_year,
+        end_year=arguments.end_year)
+    df = area_forecast_result
+    df = df.set_index(['building_category', 'TEK', 'building_condition', 'year'])
+    if building_conditions:
+        df = df.loc[:, :, [str(s) for s in building_conditions]]
+    if arguments.tek:
+        tek_in_index = [t for t in arguments.tek if any(df.index.isin([t], level=1))]
+        df = df.loc[:, tek_in_index, :]
+    if 'energy-requirements' in step_choice or 'heating-systems' in step_choice:
+        energy_requirements_result = calculate_building_category_energy_requirements(
+            building_category=building_category,
+            area_forecast=df,
+            database_manager=database_manager,
+            start_year=arguments.start_year,
+            end_year=arguments.end_year)
+        df = energy_requirements_result
+
+        if 'heating-systems' in step_choice and building_category != BuildingCategory.STORAGE_REPAIRS:
+            df = calculate_heating_systems(energy_requirements=energy_requirements_result,
+                                           database_manager=database_manager)
+    return df
+
 
 def write_result(output_file, csv_delimiter, output):
     logger.debug(f'Writing to {output_file}')

9c645da2a83e170d5211279a380fea641686c4cc
kenord <kenord@nve.no>
2025-02-25 12:39:22 +0100

    Improve what_python.py
     - Show PATH from environment
     - Split PYTHONPATH
     - Add PYTHONPATH

--- scripts/what_python.py
+++ scripts/what_python.py
@@ -3,10 +3,23 @@ import shutil
 import sys
 
 
+def split_and_break_path(path):
+    delimiter = ''
+    if ';' in path:
+        delimiter = ';'
+    elif ':' in path:
+        delimiter = ':'
+    if delimiter:
+        path = '\n' + '\n'.join([f'                {p}' for p in path.split(delimiter)])
+    return path
+
+
 python_version = sys.version_info.major, sys.version_info.minor
 conda_env = os.environ.get('CONDA_DEFAULT_ENV', 'No conda')
 exe_path = shutil.which('python.exe')
 venv_path = os.environ.get('VIRTUAL_ENV', 'No venv 🙅')
+env_python_path = os.environ.get('PYTHONPATH', 'No PYTHONPATH 🙅')
+env_path = os.environ.get('PATH', 'No PATH 🙅')
 
 conda_reaction = '😊'
 if conda_env.upper() == 'base':
@@ -20,8 +33,13 @@ if sys.version_info.minor < 11:
 elif sys.version_info.minor > 12:
     version_reaction = '😒'
 
-print('Current path', os.getcwd())
-print('Where is python.exe?', exe_path)
-print(f'Python is version {sys.version_info.major}.{sys.version_info.minor} {version_reaction}')
-print('What venv?', venv_path)
-print('What conda?', conda_env, conda_reaction)
+env_python_path = split_and_break_path(env_python_path)
+env_path = split_and_break_path(env_path)
+
+print('           CWD?', os.getcwd())
+print('    PYTHONPATH?', env_python_path)
+print('          PATH?', env_python_path)
+print('    python.exe?', exe_path)
+print(f'Python version? {sys.version_info.major}.{sys.version_info.minor} {version_reaction}')
+print('     What venv?', venv_path)
+print('    What conda?', conda_env, conda_reaction)

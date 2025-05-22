c45191059a0811d031a8582647e202fc39bd5d31
kenord <kenord@nve.no>
2025-05-22 15:58:59 +0200

    #2790 Reorganize imports

--- ebm/cmd/run_all.py
+++ ebm/cmd/run_all.py
@@ -9,14 +9,14 @@ from ebm import extractors
 from ebm.cmd.helpers import load_environment_from_dotenv
 from ebm.cmd.result_handler import transform_model_to_horizontal, transform_to_sorted_heating_systems
 from ebm.cmd.run_calculation import configure_loglevel
-import ebm.model.area as a_f
+from ebm.model import area as a_f
 from ebm.model.data_classes import YearRange
 from ebm.model.database_manager import DatabaseManager
-import ebm.model.energy_need as e_n
+from ebm.model import energy_need as e_n
 from ebm.model import energy_purpose as e_p
 from ebm.model import energy_use as e_u
 from ebm.model.file_handler import FileHandler
-import ebm.model.heating_systems_parameter as h_s_param
+from ebm.model import heating_systems_parameter as h_s_param
 from ebm.model import heat_pump as h_p
 from ebm.model.heating_systems_share import transform_heating_systems_share_long, transform_heating_systems_share_wide
 from ebm.services.spreadsheet import make_pretty, add_top_row_filter
@@ -76,36 +76,35 @@ def main():
 
     logger.debug('Transform fane 2')
     heating_systems_share = transform_heating_systems_share_long(heating_systems_projection)
+
     logger.debug('Transform fane 1')
     heating_systems_share_wide = transform_heating_systems_share_wide(heating_systems_share)
-    heating_systems_share_long = heating_systems_share.rename(columns={'TEK_shares': 'Share',
-                                                                  'heating_systems': 'Heating system'})
+    heating_systems_share_long = heating_systems_share.rename(columns={
+        'TEK_shares': 'Share',
+        'heating_systems': 'Heating system'})
+
     heating_systems_share_wide = heating_systems_share_wide.rename(columns={'heating_systems':'Heating technology'})
 
     logger.debug('Write file heating_system_share.xlsx')
     heating_system_share_file = output_path / 'heating_system_share.xlsx'
-
     with pd.ExcelWriter(heating_system_share_file, engine='xlsxwriter') as writer:
         heating_systems_share_long.to_excel(writer, sheet_name='long', merge_cells=False) # 💾
         heating_systems_share_wide.to_excel(writer, sheet_name='wide', merge_cells=False, index=False) # 💾
-
     make_pretty(heating_system_share_file)
     logger.debug(f'Adding top row filter to {heating_system_share_file}')
     add_top_row_filter(workbook_file=heating_system_share_file, sheet_names=['long'])
     logger.info(f'Wrote {heating_system_share_file.name}')
 
     logger.info('heat_prod_hp')
-
     logger.debug('Transform heating_system_parameters')
     heating_systems_parameter = h_s_param.heating_systems_parameter_from_projection(heating_systems_projection)
-    logger.debug('Transform to hp')
 
+    logger.debug('Transform to hp')
     expanded_heating_systems_parameter = h_s_param.expand_heating_system_parameters(heating_systems_parameter)
     air_air = h_p.air_source_heat_pump(expanded_heating_systems_parameter)
     district_heating = h_p.district_heating_heat_pump(expanded_heating_systems_parameter)
 
     production = h_p.heat_pump_production(total_energy_need, air_air, district_heating)
-
     heat_prod_hp_wide = h_p.heat_prod_hp_wide(production)
 
     logger.debug('Write file heat_prod_hp.xlsx')
@@ -132,17 +131,12 @@ def main():
         by=['building_category', 'TEK', 'energy_product', 'year']).sum() / 1_000_000
     energy_use_long = energy_use_long.reset_index()[column_order].rename(columns={'kwh': 'energy_use'})
 
-
     logger.debug('Transform fane 1')
-
     logger.debug('Group by group, product year')
-
     energy_use_wide = transform_to_sorted_heating_systems(energy_use_gwh_by_building_group, energy_use_holiday_homes,
                                                           building_column='building_group')
     logger.debug('Write file energy_use')
-
     energy_use_file = output_path / 'energy_use.xlsx'
-
     with pd.ExcelWriter(energy_use_file, engine='xlsxwriter') as writer:
         energy_use_long.to_excel(writer, sheet_name='long', index=False) # 💾
         energy_use_wide.to_excel(writer, sheet_name='wide', index=False) # 💾
@@ -153,12 +147,12 @@ def main():
 
     logger.debug('Transform fane 1')
     energy_purpose_wide = e_p.group_energy_use_kwh_by_building_group_purpose_year_wide(energy_use_kwh=energy_use_kwh)
+
     logger.debug('Transform fane 2')
     energy_purpose_long = e_p.group_energy_use_by_year_category_tek_purpose(energy_use_kwh=energy_use_kwh)
 
     logger.debug('Write file energy_purpose.xlsx')
     energy_purpose_output = output_path / 'energy_purpose.xlsx'
-
     with pd.ExcelWriter(energy_purpose_output, engine='xlsxwriter') as writer:
         energy_purpose_wide.to_excel(writer, sheet_name='wide', index=False) # 💾
         energy_purpose_long.to_excel(writer, sheet_name='long', index=False) # 💾
@@ -167,7 +161,6 @@ def main():
     add_top_row_filter(workbook_file=energy_purpose_output, sheet_names=['long'])
     logger.info(f'Wrote {energy_purpose_output.name}')
 
-
     area_change = a_f.transform_area_forecast_to_area_change(area_forecast=area_forecast)
 
     logger.info('demolition_construction')
@@ -175,8 +168,9 @@ def main():
     demolition_construction_long = a_f.transform_demolition_construction(energy_use_kwh, area_change)
     demolition_construction_long = demolition_construction_long.rename(columns={'m2': 'Area [m2]',
                                                                       'gwh': 'Energy use [GWh]'})
-    demolition_construction_file = output_path / 'demolition_construction.xlsx'
+
     logger.debug('Write file demolition_construction.xlsx')
+    demolition_construction_file = output_path / 'demolition_construction.xlsx'
     with pd.ExcelWriter(demolition_construction_file, engine='xlsxwriter') as writer:
         demolition_construction_long.to_excel(writer, sheet_name='long', index=False) # 💾
     make_pretty(demolition_construction_file)

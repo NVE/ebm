3055514c672666a7e26874f34cec0f83f84c1711
kenord <kenord@nve.no>
2025-06-25 13:28:42 +0200

    #2805 Make sure calculate_s_curve sum of conditions is 1.0

--- tests/ebm/test_scurve.py
+++ tests/ebm/test_scurve.py
@@ -301,6 +301,12 @@ def test_calculate_s_curves_return_columns(scurves_parameters_house, tek10_tek69
     assert 's_curve_demolition' in result.columns
 
 
+def test_calculate_s_curves_conditions_sums_to_one(scurves_parameters_house, tek10_tek69_parameters, years):
+    result = s_curve.calculate_s_curves(scurves_parameters_house, tek10_tek69_parameters, years)
+
+    assert (result.s_curve_sum.round(11) == 1.0).all()
+
+
 if __name__ == "__main__":
     import sys
     pytest.main([sys.argv[0]])
